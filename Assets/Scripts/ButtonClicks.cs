8e6c18e19cb25168a3e44cd58e3859ae7f39d31f
DESKTOP-9E8U56S\Anton Pushkin <anton.pushkin2@gmail.com>
2018-08-20 16:36:46 +0300

    Added new scene with menu

---
+++ Assets/Scripts/ButtonClicks.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClicks : MonoBehaviour {
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("CameraScene");
+    }
+
+    public void Enlarge()
+    {
+        GameObject objects = GameObject.FindGameObjectWithTag("Sushi");
+        objects.transform.localScale *= 2;
+    }
+
+}
